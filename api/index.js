4ff2d99da6f08a6004bad557459b0bf07b0b8eb6
Tray <wwwtrayfrost@gmail.com>
2020-06-22 02:18:38 +0300

    Added port variable for back

--- api/index.js
+++ api/index.js
@@ -2,6 +2,8 @@ const express = require('express')
 const app = express()
 const bodyParser = require('body-parser')
 
+const port = 3000
+
 app.use(bodyParser.urlencoded({extended: false}))
 app.use(bodyParser.json())
 app.use(require('cors')());
@@ -10,6 +12,6 @@ app.get('/*', (req, res, next) => {
     res.send(require('./db.json'))
 })
 
-app.listen(3000, function () {
-    console.log("API has been started!");
+app.listen(port, function () {
+    console.log(`API has been started on ${port}!`);
 });
